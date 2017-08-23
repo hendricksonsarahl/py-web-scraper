f4033316453440f5bcf8d1e10d2f18efbc4e7b9d
Sarah Hendrickson <hendricksonsarahl@gmail.com>
2017-08-23 11:48:13 -0700

    Added note to comments re static search

--- govscrape.py
+++ govscrape.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 from bs4.dammit import EncodingDetector
 
-# download the webpage via Requests
+# download the webpage via Requests (this particular page only works when downloaded, search is not static)
 url = 'http://bioguide.congress.gov/biosearch/biosearch1.asp'
 response = requests.get(url)
 
@@ -27,9 +27,9 @@ table = soup.find('tbody', attrs={'class': 'stripe'})
 
 # extract the data we want and form it into a new list
 list_of_rows = [] 
-for row in table.findAll('a'):
+for row in table.findAll('tr'):
     list_of_cells = []
-    for cell in row.findAll('a'):
+    for cell in row.findAll('td'):
         text = cell.text.replace('&nbsp;', '')
         list_of_cells.append(text)
     list_of_rows.append(list_of_cells)
